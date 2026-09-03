cd059b60a209e0ea93dfb9f69ad7f75c48d3d4ec
Naynay88-coder <naysha.putri228@gmail.com>
2026-09-03 10:28:49 +0700

    Add files via upload

---
+++ Pekan1/Program1_2611532028.py
@@ -0,0 +1,13 @@
+# Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a\n" 
++ "Naysha dan 2611532028.")
